0e14499ecfbe6f639028395d70f3649b68f80e4b
Chris Walls <cwalls2908@gmail.com>
2018-01-10 13:45:44 -0500

    Updating hardware owner script

--- Populate Hardware Owners/hardware_owners.rb
+++ Populate Hardware Owners/hardware_owners.rb
@@ -19,34 +19,22 @@ def log_to_csv(row: , filename: DEFAULT_FILENAME)
 end
 
 
-def import_other_asset(row: )
-	other_asset = {
-		other_asset: {
-			name: row['Name'],
-			description: row['Description'],
-			asset_id: row['Asset ID'],
-			status: {name: row['Status']},
-			asset_type: {name: row['Asset Type']},
-			ip_address: row['IP'],
-			manufacturer: row['Manufacturer'],
-			model: row['Model'],
-			serial_number: row['Serial Number'],
-			owner: {email: row['Owner']},
-			user: {email: row['User']},
-			site: {name: row['Site']},
-			department: {name: row['Department']},
-			custom_fields_values: {
-				custom_fields_value: [
-					{name: 'Cost', value: row['Cost']},
-					{name: 'Purchased From', value: row['Purchased From']}
-				]
+def assign_hardware_owner(row: )
+	hardware_id = @samanage.find_hardware_id_by_serial(serial_number: row['Serial Number'])
+	if hardware_id
+		hardware_owner_update = {
+			hardware: {
+				owner: {email: row['Owner']}
 			}
 		}
-	}
-	@samanage.create_other_asset(payload: other_asset)
+		@samanage.create_other_asset(payload: hardware_owner_update)
+	else
+		row['Error'] = 'Hardware not found'
+		log_to_csv(row: row.to_h.values)
+	end
 	rescue => e
 		log_to_csv(row: row.to_h.values)
 end
 
 
-csv_rows.map{|row| import_other_asset(row: row)}
+csv_rows.map{|row| assign_hardware_owner(row: row)}
