9607c721571539689582755cee2b4e5f153b224b
Chris Walls <cwalls2908@gmail.com>
2018-01-12 12:36:37 -0500

    Adding ruby warranty

---
+++ Sync Warranty/ruby/README.md
@@ -0,0 +1,21 @@
+# Samanage Warranty Sync Script
+
+
+This simple script allows you to sync warranty information into Samanage using Python.
+
+## Requirements
+- [Ruby 2.3+]
+- [Samanage Gem](https://rubygems.org/gems/samanage) for Ruby
+- CSV of Warranty information (Use our attached template!)
+- Native Samanage API Token (single sign on credentials are not supported)
+	
+
+## Instructions
+- Compile your warranty information into the above **warranties.csv** template
+	- US datacenter: ```ruby sync_warranties.rb warranties.csv API_TOKEN```
+	- EU datacenter: ```ruby sync_warranties.rb warranties.csv API_TOKEN eu```
+
+
+### Notes
+- Dates are parsed explicitly using the format 01/31/2000. If you wish to use a different date format this can be modified here:
+- All warranties included in the CSV will be uploaded. Please exclude any expired warranties if you do not wish to import them.
--- Sync Warranty/ruby/sync_warranties.rb
+++ Sync Warranty/ruby/sync_warranties.rb
@@ -11,36 +11,49 @@ csv_rows = CSV.read(input, headers: true, encoding: 'ISO-8859-1')
 OUTPUT_HEADERS = csv_rows.headers << 'Error'
 DEFAULT_FILENAME = "Errors-#{input.split('.').first}-#{DateTime.now.strftime("%b-%d-%Y-%l%M")}.csv"
 
-def log_to_csv(row: , filename: DEFAULT_FILENAME, headers: OUTPUT_HEADERS)
+def log_to_csv(message: , filename: DEFAULT_FILENAME, headers: OUTPUT_HEADERS)
     CSV.open(filename, 'a+', write_headers: true, force_quotes: true) do |csv| 
       csv << OUTPUT_HEADERS if csv.count.eql? 0
-      csv << incident
+      csv << message
     end
 end
 
 
+def set_status(start_date, end_date)
+  if end_date < Date.today
+    'Expired'
+  elsif (start_date..end_date).include?(Date.today)
+    'Active'
+  else
+    'Future'
+  end
+end
+
+
 def sync_warranty(warranty: )
-  hardware_id = @samanage.find_hardware_by_serial(serial_number: row['Serial Number'])
-  start_date = Date.parse(warranty['Start_date'])
-  end_date = Date.parse(warranty['End Date'])
-  status = (start_date..end_date).include?(Date.today) ? true : false
+  hardware = @samanage.find_hardwares_by_serial(serial_number: warranty['Serial Number'])[:data]
+  puts "#{hardware}"
+  hardware_id = hardware.select{|h| h.dig('serial_number') == warranty['Serial Number']}.first.to_h.dig('id')
+  puts "hardware_id: #{hardware_id}"
+  start_date = Date.strptime(warranty['Start Date'], "%m/%d/%Y")
+  end_date = Date.strptime(warranty['End Date'], '%m/%d/%Y')
+  status = set_status(start_date, end_date)
   if hardware_id
     warranty = {
       warranty:{
         service:warranty['Service'],
         provider:warranty['Provider'],
-        start_date:warranty['Start Date'],
-        end_date:warranty['End Date'],
+        start_date:start_date,
+        end_date:end_date,
         status: status,
       }
     }
-    @samanage.execute(
-      http_method: 'post',
-      path: "hardwares/#{hardware_id}/warranties.json",
-      payload: warranty
-    )
+    puts warranty.inspect
+    @samanage.execute(http_method: 'post',path: "hardwares/#{hardware_id}/warranties.json",payload: warranty)
+  else
+    puts "Hardware serial: #{warranty['Serial Number']} not found"
   end
-  rescue => e
+rescue Samanage::Error, Samanage::InvalidRequest => e
     warranty['Error'] = "#{e.class}: #{e}"
     log_to_csv(message: warranty.values)  
 end
