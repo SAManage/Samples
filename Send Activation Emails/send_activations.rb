9b975324767801676df0f44c2686f4e7c7213f6e
Chris Walls <cwalls2908@gmail.com>
2018-09-28 13:57:09 -0400

    Updating activation example

--- Send Activation Emails/send_activations.rb
+++ Send Activation Emails/send_activations.rb
@@ -1,6 +1,15 @@
 require 'samanage'
 
 @token = ARGV[0]
-@api_controller = Samanage::Api.new(token: @token)
-users = @api_controller.users
-users.reject!{|u| u['last_login']}.map{|u| @api_controller.send_activation_email(email: u['email'])}
+@samanage = Samanage::Api.new(token: @token)
+@samanage.users.each do |user|
+  user_needs_to_be_activated = !user['last_login'] && !user['disabled']
+  if user_needs_to_be_activated
+    begin
+      puts "Sending Activation to #{user['email']}"
+      @samanage.send_activation_email(email: user['email'])
+    rescue Samanage::Error, Samanage::InvalidRequest => e
+      puts "[#{e}] #{e.status_code} - #{e.response}"
+    end
+  end
+end
